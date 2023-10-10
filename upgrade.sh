6e3b6e7dbd5af1d15606df1c4f623676fa312f42
Michael Lugassy <mlugassy@kehalim.com>
2023-10-10 21:34:13 +0300

    faster upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /root
-curl -o main.zip https://codeload.github.com/mluggy/teslabox/zip/refs/heads/main
+curl --parallel -o main.zip https://codeload.github.com/mluggy/teslabox/zip/refs/heads/main
 unzip -o main.zip
 cp -r teslabox-main/* teslabox
 rm -rf teslabox-main
